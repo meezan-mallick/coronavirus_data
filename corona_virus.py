4d6ff0c13d9185f2c36840b4eba3d09aa4f106db
Meezan Malek <meezanmalek1234@gmail.com>
2020-04-28 16:23:28 +0530

    sorting the highest cases

--- corona_virus.py
+++ corona_virus.py
@@ -3,44 +3,10 @@
 
 # # Python script to get corona virus data by Meezan Malek
 
-# In[29]:
-
-
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
-# In[44]:
-
-
-source = requests.get('https://www.mohfw.gov.in/').text
-soup = BeautifulSoup(source,'lxml')
-
-
-mylist = list()
-
-table = soup.find('table',class_="table table-striped")
-
-with open('Corona_virus.txt','w') as r:
-    r.write("N0".ljust(30))
-    r.write("State".ljust(30))
-    r.write("Total Confirmed cases".ljust(30))
-    r.write("Cured/Discharged".ljust(30))
-    r.write("Death".ljust(30))
-    
-    
-    for row in table.find_all('tr'):
-        for cell in row.find_all('td'):
-            r.write(cell.text.ljust(30))
-        r.write('\n')
-        
-print("data saved in text file : corona_virus.txt")
-
-
-# In[36]:
-
 
 source = requests.get('https://www.mohfw.gov.in/').text  #source of the covid-19 website
 soup = BeautifulSoup(source,'lxml')  #html parser 
